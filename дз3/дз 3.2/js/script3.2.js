6f7c35f26288f7eec10db962e2720664d206356f
Valentina1 <Valentina1>
2017-04-14 01:43:34 +0300

    ITEA-Home-Works

--- дз3/дз 3.2/js/script3.2.js
+++ дз3/дз 3.2/js/script3.2.js
@@ -1,11 +1,13 @@
 var numbers = [];
+
 while (true) {
 	var value = prompt("Введіть число", 0);
 	if (value === "" || value === null || isNaN(value)) break;
 	numbers.push(+value);
 }
 var sum = 0;
+
 for (var i = 0; i < numbers.length; i++) {
-  sum += numbers[i];
+	sum += numbers[i];
 }
 alert( sum );
