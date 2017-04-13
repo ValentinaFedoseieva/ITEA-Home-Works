82dc24d516a91b122ee831c773b1ed7ba1ad4165
Valentina1 <Valentina1>
2017-04-14 01:22:38 +0300

    ITEA-Home-Works

--- дз2/дз 2.2/js/script2.2.js
+++ дз2/дз 2.2/js/script2.2.js
@@ -2,10 +2,10 @@ var x = prompt('Введіть перше число', '');
 var y = prompt('Введіть друге число', '');
 var c = +x - +y;
 var d = +x + +y;
-var z =x*y;
+var z = x * y;
 alert(z);
-if (x>y) {
+if (x > y) {
 	alert (c);
-} else if (x<y) {
+} else if (x < y) {
 	alert (d);
 }
